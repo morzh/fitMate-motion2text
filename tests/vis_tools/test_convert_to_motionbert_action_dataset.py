4302e18169603f36b22a16a245122f7aefbf63b5
anton <morzhakovanton@gmail.com>
2023-09-18 22:57:17 +0300

    PCT dataset for motionBERT unit test

--- tests/vis_tools/test_convert_to_motionbert_action_dataset.py
+++ tests/vis_tools/test_convert_to_motionbert_action_dataset.py
@@ -1,40 +1,119 @@
+import unittest
 import numpy as np
-from vis_tools.convert_to_motionbert_action_dataset import DatasetClampDivideStrategy
-
-'''
-Test the following cases
-1.      NUMBER SAMPLES == 243
-
-2.      NUMBER SAMPLES < 243
-2.a.    number samples < minimum_number_frames
-2.b.    minimum_number_frames <= number samples < 243 - 2*number_cutoff_frames
-2.c.    243 - 2*number_cutoff_frames <= number samples < 243 
-
-3.      NUMBER SAMPLES > 243
-3.a.    243 < number samples <  243 + 2*number_cutoff_frames
-3.b.    243 + 2*number_cutoff_frames <= number samples
-'''
-
-number_cutoff_frames = 30
-minimum_number_frames = 50
-sample_frames = 243
-dataset_generator = DatasetClampDivideStrategy(number_cutoff_frames)
-
-sample_1 = np.random.random((sample_frames, 17, 3))
-
-sample_2a = np.random.random((np.random.randint(0, minimum_number_frames), 17, 3))
-sample_2b = np.random.random((np.random.randint(minimum_number_frames, sample_frames - 2 * number_cutoff_frames), 17, 3))
-sample_2c = np.random.random((np.random.randint(sample_frames - 2 * number_cutoff_frames, sample_frames), 17, 3))
-
-sample_3a = np.random.random((np.random.randint(sample_frames, sample_frames + 2 * number_cutoff_frames), 17, 3))
-sample_3b = np.random.random((np.random.randint(sample_frames + 2 * number_cutoff_frames, int(1e4)), 17, 3))
+from vis_tools.convert_to_motionbert_action_dataset import DatasetFromVideo
 
 
-sample_1_motionbert = dataset_generator.get_samples(sample_1)
+class TestDatasetFromVideo(unittest.TestCase):
+    """
+    Test the following cases
+    1.      NUMBER FRAMES < sample_frames_number
+    1.a.    number samples < minimum_number_frames
+    1.b.    minimum_number_frames <= number samples < sample_frames_number
 
-sample_2a_motionbert = dataset_generator.get_samples(sample_2a)
-sample_2b_motionbert = dataset_generator.get_samples(sample_2b)
+    2.      NUMBER FRAMES == sample_frames_number
 
-sample_3a_motionbert = dataset_generator.get_samples(sample_3a)
-sample_3b_motionbert = dataset_generator.get_samples(sample_3b)
-sample_3c_motionbert = dataset_generator.get_samples(sample_3c)
+    3.      NUMBER FRAMES > sample_frames_number
+    3.a.    sample_frames_number < number samples <  sample_frames_number + 2*number_cutoff_frames
+    3.b.    sample_frames_number + 2*number_cutoff_frames <= number samples
+    """
+    def setUp(self):
+        self.number_cutoff_frames = 30
+        self.minimum_number_frames = 90
+        self.sample_frames_number = 243
+        self.number_joints = 17
+        self.dataset_from_video = DatasetFromVideo(number_cutoff_frames=self.number_cutoff_frames,
+                                                   minimum_number_frames=self.minimum_number_frames,
+                                                   sample_frames_number=self.sample_frames_number,
+                                                   number_joints=self.number_joints)
+        self.number_tests = 1500
+
+    def test_case_1a(self):
+        """
+            Case 1.a.
+            number samples < minimum_number_frames
+        Returns:
+            should return empty numpy array, shape[0] = 0
+        """
+        for index in range(self.number_tests):
+            sample_1a_in = np.random.random((np.random.randint(0, self.minimum_number_frames), self.number_joints, 3))
+            sample_1a_out = self.dataset_from_video.get_samples(sample_1a_in)
+            self.assertEqual(sample_1a_out.shape[0], 0)
+
+    def test_case_1b(self):
+        """
+            Case 1.b.
+            self.minimum_number_frames <= number_frames < sample_frames_number
+        Returns:
+
+        """
+        for index in range(self.number_tests):
+            sample_1b_in = np.random.random((np.random.randint(self.minimum_number_frames, self.sample_frames_number), self.number_joints, 3))
+            sample_1b_out = self.dataset_from_video.get_samples(sample_1b_in)
+
+            number_frames = sample_1b_in.shape[0]
+            number_additional_frames = self.sample_frames_number - number_frames
+            number_pre_additional_frames = int(np.ceil(0.5 * number_additional_frames))
+            number_post_additional_frames = int(np.floor(0.5 * number_additional_frames))
+
+            self.assertEqual(sample_1b_out.shape[0], 2)
+            self.assertEqual(sample_1b_out.shape[1], self.sample_frames_number)
+            self.assertTrue((sample_1b_out[0][:number_pre_additional_frames] == sample_1b_in[0]).all())
+            self.assertTrue((sample_1b_out[0][-number_post_additional_frames-1:] == sample_1b_in[-1]).all())
+
+    def test_case_2(self):
+        """
+            Case 2.
+            number_frames == sample_frames_number
+        Returns:
+
+        """
+        for index in range(self.number_tests):
+            sample_2_in = np.random.random((self.sample_frames_number, self.number_joints, 3))
+            sample_2_out = self.dataset_from_video.get_samples(sample_2_in)
+
+            self.assertEqual(sample_2_out.shape[0], 2)
+            self.assertEqual(sample_2_out.shape[1], self.sample_frames_number)
+
+    def test_case_3a(self):
+        """
+            Case 3.a.
+            sample_frames_number < number samples < sample_frames_number + 2 * number_cutoff_frames
+        Returns:
+
+        """
+        for index in range(self.number_tests):
+            sample_3a_in = np.random.random((np.random.randint(self.sample_frames_number + 1, self.sample_frames_number + 2 * self.number_cutoff_frames - 1), self.number_joints, 3))
+            sample_3a_out = self.dataset_from_video.get_samples(sample_3a_in)
+
+            number_frames = sample_3a_in.shape[0]
+            number_additional_frames = number_frames - self.sample_frames_number
+            number_pre_additional_frames = int(np.ceil(0.5 * number_additional_frames))
+            number_post_additional_frames = int(np.floor(0.5 * number_additional_frames))
+
+            if number_post_additional_frames == 0:
+                sample_check = sample_3a_in[number_pre_additional_frames:]
+            else:
+                sample_check = sample_3a_in[number_pre_additional_frames: -number_post_additional_frames]
+
+            self.assertEqual(sample_3a_out.shape[0], 2)
+            self.assertEqual(sample_3a_out.shape[1], self.sample_frames_number)
+            self.assertTrue((sample_check == sample_3a_out[0]).all())
+
+    def test_case_3b(self):
+        """
+            Case 3.b.
+            sample_frames_number + 2*number_cutoff_frames < number samples
+        Returns:
+        """
+        for index in range(self.number_tests):
+            sample_3b_in = np.random.random((np.random.randint(self.sample_frames_number + 2 * self.number_cutoff_frames, int(1e4)), self.number_joints, 3))
+            sample_3b_out = self.dataset_from_video.get_samples(sample_3b_in)
+
+            number_frames = sample_3b_in.shape[0]
+            number_samples = (number_frames - 2 * self.number_cutoff_frames) // self.sample_frames_number
+            samples = sample_3b_in[self.number_cutoff_frames: self.number_cutoff_frames + number_samples * self.sample_frames_number]
+
+            for index_sample in range(number_samples - 1):
+                self.assertTrue((sample_3b_out[index_sample][0] == samples[index_sample * self.sample_frames_number:  (index_sample + 1) * self.sample_frames_number]).all())
+
+
--- vis_tools/convert_to_motionbert_action_dataset.py
+++ vis_tools/convert_to_motionbert_action_dataset.py
@@ -13,57 +13,82 @@ class DatasetStrategy(object):
         pass
 
 
-class DatasetClampDivideStrategy(DatasetStrategy):
-    def __init__(self, soft_cutoff_frames: int,  minimum_number_frames=50, sample_frames_length=243, number_joints=17):
-        super().__init__()
-        self.soft_cutoff_frames_ = soft_cutoff_frames
-        self.minimum_number_frames_ = minimum_number_frames
-        self.sample_frames_length_ = sample_frames_length
-        self.number_joints_ = number_joints
-        self.second_person_fake_ = np.zeros((1, self.sample_frames_length_, self.number_joints_, 3))
-
-    def get_samples(self, data: np.ndarray) -> np.ndarray:
+class DatasetFromVideo(DatasetStrategy):
+    def __init__(self, number_cutoff_frames=30, minimum_number_frames=50, sample_frames_number=243, number_joints=17):
         """
 
         Args:
-            data:
+            number_cutoff_frames:
+            minimum_number_frames:
+            sample_frames_number:
+            number_joints:
+        """
+        super().__init__()
+        self.soft_cutoff_frames_ = number_cutoff_frames
+        self.minimum_number_frames_ = minimum_number_frames
+        self.sample_frames_number_ = sample_frames_number
+        self.number_joints_ = number_joints
+        self.second_person_fake_ = np.zeros((1, self.sample_frames_number_, self.number_joints_, 3))
+
+    def get_samples(self, data: np.ndarray) -> np.ndarray:
+        """
+        The following cases are considered:
+            1.      NUMBER FRAMES < self.sample_frames_length_
+            1.a.    number_samples < self.minimum_number_frames
+            1.b.    self.minimum_number_frames <= number_samples < self.sample_frames_length_
+
+            2.      NUMBER FRAMES == self.sample_frames_length_
+
+            3.      NUMBER FRAMES > self.sample_frames_length_
+            3.a.    self.sample_frames_length_ < number_frames <  self.sample_frames_length_ + 2*self.number_cutoff_frames
+            3.b.    self.sample_frames_length_ + 2*number_cutoff_frames <= number_frames
+
+        Args:
+            data: numpy array in [frames, joints, 3]  format
 
         Returns:
-            numpy array in [number_samples, 2, number_frames, number_joints, 3] format.
-            It assumes two persons are interacting (second person could be faked)
+            numpy array in [number_samples, 2, number_frames, number_joints, 3] format, number_samples >= 0.
+            It assumes two persons are interacting (second person is a faked person)
         """
         number_frames = data.shape[0]
-        if number_frames > self.sample_frames_length_:
-            if (number_frames - 2 * self.soft_cutoff_frames_) < self.sample_frames_length_:
-                number_new_frames = number_frames - self.sample_frames_length_
-                number_frames_pre_cut = np.ceil(number_new_frames)
-                number_frames_post_cut = np.floor(number_new_frames)
-                sample = data[number_frames_pre_cut: number_frames_post_cut]
-                return self.fake_second_person_(sample)
-            elif (number_frames - 2 * self.soft_cutoff_frames_) == self.sample_frames_length_:
-                sample = data[self.soft_cutoff_frames_, -self.soft_cutoff_frames_]
-                return self.fake_second_person_(sample)
-            else:
-                number_samples = (number_frames - 2 * self.soft_cutoff_frames_) % self.sample_frames_length_
-                samples = np.zeros((number_samples, 2, self.sample_frames_length_, self.number_joints_, 3))
-                for sample_index in range(number_samples):
-                    index_first = self.soft_cutoff_frames_ + sample_index * self.sample_frames_length_
-                    index_second = self.soft_cutoff_frames_ + (sample_index + 1) * self.sample_frames_length_
-                    sample = data[index_first: index_second]
-                    samples[sample_index] = self.fake_second_person_(sample)
-                return samples
-        elif number_frames == self.sample_frames_length_:
-            return self.fake_second_person_(data)
-        else:
-            if number_frames < self.minimum_number_frames_:
-                return np.empty((0, 2, 243, 17, 3))
-            number_new_frames = number_frames - self.sample_frames_length_
-            number_frames_pre_add = np.ceil(number_new_frames)
-            number_frames_post_add = np.floor(number_new_frames)
-            sample_pre_add = np.repeat(data[0], number_frames_pre_add)
-            sample_post_add = np.repeat(data[-1], number_frames_post_add)
+
+        if number_frames < self.minimum_number_frames_:  # case 1.a
+            return np.empty((0, 2, 243, 17, 3))
+        elif self.minimum_number_frames_ <= number_frames < self.sample_frames_number_:  # case 1.b
+            number_additional_frames = self.sample_frames_number_ - number_frames
+            number_pre_additional_frames = int(np.ceil(0.5 * number_additional_frames))
+            number_post_additional_frames = int(np.floor(0.5 * number_additional_frames))
+            sample_pre_add = np.repeat(np.expand_dims(data[0], axis=0), number_pre_additional_frames, axis=0)
+            sample_post_add = np.repeat(np.expand_dims(data[-1], axis=0), number_post_additional_frames, axis=0)
             sample = np.vstack((sample_pre_add, data, sample_post_add))
             return self.fake_second_person_(sample)
+        elif number_frames == self.sample_frames_number_:  # case 2.
+            return self.fake_second_person_(data)
+        elif self.sample_frames_number_ < number_frames < self.sample_frames_number_ + 2 * self.soft_cutoff_frames_:  # case 3.a
+            number_additional_frames = number_frames - self.sample_frames_number_
+            number_pre_additional_frames = int(np.ceil(0.5 * number_additional_frames))
+            number_post_additional_frames = int(np.floor(0.5 * number_additional_frames))
+            if number_post_additional_frames == 0:
+                sample = data[number_pre_additional_frames:]
+            else:
+                sample = data[number_pre_additional_frames: -number_post_additional_frames]
+            return self.fake_second_person_(sample)
+        else:  # case 3.b
+            number_samples = (number_frames - 2 * self.soft_cutoff_frames_) // self.sample_frames_number_
+            samples = data[self.soft_cutoff_frames_: self.soft_cutoff_frames_ + number_samples * self.sample_frames_number_]
+            samples = samples.reshape(number_samples, self.sample_frames_number_, self.number_joints_, 3, order='C')
+            samples = np.expand_dims(samples, axis=1)
+            zeros = np.zeros(samples.shape)
+            samples = np.hstack((samples, zeros))
+            '''
+            samples = np.zeros((number_samples, 2, self.sample_frames_number_, self.number_joints_, 3))
+            for sample_index in range(number_samples):
+                index_first = self.soft_cutoff_frames_ + sample_index * self.sample_frames_number_
+                index_second = self.soft_cutoff_frames_ + (sample_index + 1) * self.sample_frames_number_
+                sample = data[index_first: index_second]
+                samples[sample_index] = self.fake_second_person_(sample)
+            '''
+            return samples
 
     def fake_second_person_(self,  data: np.ndarray) -> np.ndarray:
         sample = np.expand_dims(data, axis=0)
@@ -80,7 +105,7 @@ if '__name__' == '__main__':
     folder_dataset = '/home/anton/work/fitMate/datasets/ActionDatasets/TestActions/motionBERT_dataset'
     files = [f for f in os.listdir(folder_annotations) if os.path.isfile(os.path.join(folder_annotations, f)) and f.endswith('json')]
 
-    dataset_generator = DatasetClampDivideStrategy(60)
+    dataset_generator = DatasetFromVideo()
 
     for file in files:
         json_filepath = os.path.join(folder_annotations, file)
